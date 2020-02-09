653febaed12c0571c6f39ae0dcd414b78c71eb8d
popuguy <d3xrow@gmail.com>
2020-02-09 19:59:32 +0000

    refactor: App

--- src/app.js
+++ src/app.js
@@ -1,43 +1,89 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from '@reach/router';
-import { create } from 'jss';
-import JssProvider from 'react-jss/lib/JssProvider';
+import { Route, useLocation } from 'wouter';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
-import CssBaseline from '@material-ui/core/CssBaseline/CssBaseline';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import makeStyles from '@material-ui/styles/makeStyles';
 
-import { configureStore } from 'store';
-import { loadState, saveState } from 'libs/localStorage';
+// icons
+import AddIcon from '@material-ui/icons/Add';
 
-import AppPage from '../app';
+// libs
+import { loadState, saveState } from '../libs/localStorage';
 
-const styleNode = document.createComment('insertion-point-jss');
-document.head.insertBefore(styleNode, document.head.firstChild);
+// store
+import { configureStore } from '../app/store';
 
-const generateClassName = createGenerateClassName();
-const jss = create(jssPreset());
-jss.options.insertionPoint = 'insertion-point-jss';
+// theming and styling
+import { theme } from '../app/theme';
+
+// pages
+import { HomePage } from '../app/pages/Home';
+import { AddBookPage } from '../app/pages/Book/AddBook';
+
+// components
+import { LinkBehavior } from '../app/components/Link';
+
 // redux store
 const persistedState = loadState();
 const store = configureStore(persistedState);
 
 store.subscribe(() => {
-  console.log('storeSave');
   saveState(store.getState());
 });
 
+const useStyles = makeStyles({
+  appBar: {
+    background: 'linear-gradient(to right, #f79d00, #64f38c)'
+  },
+  fab: {
+    position: 'fixed',
+    bottom: 50,
+    right: 50
+  },
+  typo: {
+    textDecoration: 'none'
+  }
+});
+
 export function App() {
+  const [location] = useLocation();
+  const { appBar, fab, typo } = useStyles();
+
   return (
     <Provider store={store}>
-      <Fragment>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <JssProvider jss={jss} generateClassName={generateClassName}>
-          <Router>
-            <AppPage path="/*" />
-          </Router>
-        </JssProvider>
-      </Fragment>
+        <AppBar position="static" className={appBar}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              color="inherit"
+              component={LinkBehavior}
+              to="/"
+              className={typo}
+            >
+              Book Editor
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Route path="/" component={HomePage} />
+        <Route path="/book/add" component={AddBookPage} />
+        <Route path="/book/:id/edit">{params => <AddBookPage edit id={params.id} />}</Route>
+
+        <Zoom in={location === '/'} unmountOnExit>
+          <Fab color="primary" className={fab} component={LinkBehavior} to="/book/add">
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </ThemeProvider>
     </Provider>
   );
 }

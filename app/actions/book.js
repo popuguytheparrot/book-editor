e54a7d79fe8098390ab546ee0ead90dc254d826b
popuguy <d3xrow@gmail.com>
2020-02-22 18:22:53 +0000

    change action from redux to reatom

--- app/actions/book.js
+++ app/actions/book.js
@@ -1,28 +1,6 @@
-import { ADD_BOOK, DELETE_BOOK, EDIT_BOOK, GET_BOOKS } from 'constants/actionTypes/book';
+import { declareAction } from '@reatom/core';
 
-export function getBooksAction() {
-  return {
-    type: GET_BOOKS
-  };
-}
-
-export function deleteBookAction(id) {
-  return {
-    type: DELETE_BOOK,
-    payload: { id }
-  };
-}
-
-export function addBookAction(book) {
-  return {
-    type: ADD_BOOK,
-    payload: { book }
-  };
-}
-
-export function editBookAction(book) {
-  return {
-    type: EDIT_BOOK,
-    payload: { book }
-  };
-}
+export const getBooks = declareAction();
+export const deleteBook = declareAction();
+export const addBook = declareAction();
+export const editBook = declareAction();

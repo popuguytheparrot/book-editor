96467022e2222493662f5c0b8b6b1ff51674c51c
popuguy <d3xrow@gmail.com>
2020-02-22 18:27:17 +0000

    change creator store to reatom

--- app/store/index.js
+++ app/store/index.js
@@ -1,17 +1,9 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from '@reatom/core';
 
-import { rootReducer } from 'reducers';
+import { bookAtom } from 'reducers';
 
 export function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState, composeWithDevTools());
+  const store = createStore(bookAtom, initialState);
 
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('reducers');
-      store.replaceReducer(nextRootReducer);
-    });
-  }
   return store;
 }
